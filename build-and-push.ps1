736b3a353b1034df8ad5d4a73813a8ef0c7c01f9
22127294 <hpnghia22@clc.fitus.edu.vn>
2025-09-13 08:12:03 +0700

    Init project fork from https://github.com/spring-petclinic/spring-petclinic-microservices.git and write a code to build and push image to DockerHub in Window(PowerShell)

---
+++ build-and-push.ps1
@@ -0,0 +1,46 @@
+# ===============================
+# Config
+$DOCKER_USER = "nghiax1609"
+$DOCKER_REPO_PREFIX = "$DOCKER_USER/spring-petclinic"
+$services = @(
+    "customers-service",
+    "vets-service",
+    "visits-service",
+    "api-gateway",
+    "config-server",
+    "discovery-server",
+    "admin-server"
+)
+# ===============================
+
+# Write-Host "=== Step 1: Build Docker images with Maven ==="
+# ./mvnw clean install -P buildDocker -DskipTests
+
+Write-Host "=== Step 2: Tag and Push images to Docker Hub ==="
+
+foreach ($service in $services) {
+    if ([string]::IsNullOrWhiteSpace($service)) {
+        Write-Host " Skipping empty service"
+        continue
+    }
+
+    $LOCAL_TAG = "springcommunity/spring-petclinic-$($service):latest"
+    $REMOTE_TAG = "$DOCKER_REPO_PREFIX-$($service):latest"
+
+
+    # Kiểm tra image có tồn tại chưa
+    $exists = docker images -q $LOCAL_TAG
+    if (-not $exists) {
+        Write-Host "Image $LOCAL_TAG not found. Skipping..."
+        continue
+    }
+
+    Write-Host "--- Pushing $service ---"
+    Write-Host "Tagging $LOCAL_TAG as $REMOTE_TAG"
+    docker tag $LOCAL_TAG $REMOTE_TAG
+
+    Write-Host "Pushing $REMOTE_TAG"
+    docker push $REMOTE_TAG
+}
+
+Write-Host "All images built and pushed successfully!"
